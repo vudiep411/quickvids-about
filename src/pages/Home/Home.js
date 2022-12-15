7940dd259da998fefc1624e3fbe795f260b44072
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-12-14 22:22:51 -0800

    Made home page better, fixed warnings

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,11 @@
-import { Container } from '@mui/material'
+// import { Container } from '@mui/material'
 import React from 'react'
 import './home.css'
 const Home = () => {
   return (
     <div>
-      <Container maxWidth='lg'>
-         Home
-
-      </Container>
+<h1>QuickVids</h1>
+<h4>Upload, <span class="underlined underline-clip">View</span><br></br>& React to <span class="underlined underline-mask">Exotic</span><br></br><span class="underlined underline-overflow">Videos</span></h4>
     </div>
   )
 }
